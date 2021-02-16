5d0b60558ee8bffb49d18167d124c37cdf0956f8
NickGBR <gubarenko.nikita27@gmail.com>
2021-02-16 17:38:27 +0300

    Добавлены bootstrap, контроллер принимающий UserDTO, добвлен MapStruct, протестирована работа бд при добавлении пользователя. Доавлена генерация номер банковского аккаунта (счета). Создана первая тестовая версия пользовательнокого интерфейса.

--- src/main/java/ru/interns/deposit/controller/UiController.java
+++ src/main/java/ru/interns/deposit/controller/UiController.java
@@ -1,4 +1,36 @@
 package ru.interns.deposit.controller;
 
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.interns.deposit.db.dao.Deposit;
+import ru.interns.deposit.db.repositoiry.DepositRepository;
+import ru.interns.deposit.dto.UserDTO;
+import ru.interns.deposit.mapper.UserMapper;
+
+@Controller
+@RequestMapping("app/")
 public class UiController {
+
+    private UserMapper mapper;
+    private DepositRepository repository;
+
+    @Autowired
+    public UiController(UserMapper mapper, DepositRepository repository) {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+
+
+    @PostMapping("/create")
+    public @ResponseBody Boolean create(@RequestBody UserDTO userDTO){
+        final Deposit deposit = mapper.toEntity(userDTO);
+        repository.save(deposit);
+        return true;
+    }
+
 }
