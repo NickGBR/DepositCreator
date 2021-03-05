8259128ae89451ee79a438884b769ec7a9dafb08
i.nazimov <i.nazimov@i-teco.ru>
2021-03-05 13:22:36 +0300

    ActiveMQ сервис в deposit

--- src/main/java/ru/interns/deposit/external/mvd/service/impl/MVDServiceImpl.java
+++ src/main/java/ru/interns/deposit/external/mvd/service/impl/MVDServiceImpl.java
@@ -20,10 +20,10 @@ import ru.interns.deposit.external.deposit.DepositService;
 import ru.interns.deposit.external.deposit.dto.DepositRequestDTO;
 import ru.interns.deposit.external.enums.CheckingStatus;
 import ru.interns.deposit.external.mvd.dto.MvdRequestDTO;
+import ru.interns.deposit.external.mvd.dto.MvdResultCheckingDTO;
 import ru.interns.deposit.external.mvd.enums.CheckType;
 import ru.interns.deposit.external.mvd.enums.MvdErrors;
 import ru.interns.deposit.external.mvd.service.MVDService;
-import ru.interns.deposit.mapper.PersonalDataMapper;
 import ru.interns.deposit.service.impl.PersonalDataService;
 import ru.interns.deposit.service.impl.UserService;
 
@@ -36,40 +36,47 @@ import java.util.UUID;
 public class MVDServiceImpl implements MVDService {
     private static final String DESTINATION_NAME_LISTENER = "response";
     private static final String DESTINATION_NAME_CONSUMER = "request";
+    private static final List<UUID> correlationIdList = new ArrayList<>();
 
     private final JmsTemplate jmsTemplate;
     private final DepositService depositService;
     private final UserService userService;
     private final PersonalDataService personalDataService;
-    private final PersonalDataMapper mapper;
 
     @Autowired
-    public MVDServiceImpl(JmsTemplate jmsTemplate, DepositService depositService, UserService userService, PersonalDataService personalDataService, PersonalDataMapper personalDataMapper) {
+    public MVDServiceImpl(JmsTemplate jmsTemplate, DepositService depositService, UserService userService, PersonalDataService personalDataService) {
         this.jmsTemplate = jmsTemplate;
         this.depositService = depositService;
         this.userService = userService;
         this.personalDataService = personalDataService;
-        this.mapper = personalDataMapper;
     }
 
     @JmsListener(destination = DESTINATION_NAME_LISTENER)
     public void processMessages(Message message) throws JMSException, JSONException {
-        String jsonBody = message.getStringProperty("JSONClient");
-        JSONObject jsonObject = new JSONObject(jsonBody);
-        List<MvdErrors> mvdErrors = new ArrayList<>();
+        final String jsonBody = message.getStringProperty("JSONClient");
+        final JSONObject jsonObject = new JSONObject(jsonBody);
 
-        UUID uuid = UUID.fromString(jsonObject.get("uuid").toString());
-        final String login = LoginInfoService.data.get(uuid);
+        final String correlationID = message.getJMSCorrelationID();
+        final UUID uuid = UUID.fromString(correlationID);
 
-        JSONArray errorsList = jsonObject.getJSONArray("errorsList");
-        CheckingStatus checkingStatus = jsonObject.getEnum(CheckingStatus.class, "checkingStatus");
+        if (correlationIdList.contains(uuid)) {
+            final String login = LoginInfoService.data.get(uuid);
 
-        errorsList.forEach(x -> mvdErrors.add((MvdErrors) x));
+            JSONArray errorsList = jsonObject.getJSONArray("mvdErrorsList");
+            CheckingStatus checkingStatus = jsonObject.getEnum(CheckingStatus.class, "checkingStatus");
 
-        MvdStatus.mvdCheckResult.get(login).setMvdErrorsList(mvdErrors);
-        MvdStatus.mvdCheckResult.get(login).setCheckingStatus(checkingStatus);
+            List<MvdErrors> mvdErrors = new ArrayList<>();
+            errorsList.forEach(x -> mvdErrors.add(MvdErrors.valueOf(x.toString())));
 
-        checkAndOpenDepositForUserByUuid(uuid);
+            MvdResultCheckingDTO mvdResultCheckingDTO = MvdStatus.mvdCheckResult.get(login);
+            if (mvdResultCheckingDTO != null) {
+                mvdResultCheckingDTO.setMvdErrorsList(mvdErrors);
+                mvdResultCheckingDTO.setCheckingStatus(checkingStatus);
+            }
+
+            checkAndOpenDepositForUserByUuid(uuid);
+            correlationIdList.remove(uuid);
+        }
     }
 
     @Override
@@ -92,9 +99,11 @@ public class MVDServiceImpl implements MVDService {
         message.setJMSCorrelationID(userDTO.getUuid().toString());
 
         jmsTemplate.convertAndSend(DESTINATION_NAME_CONSUMER, message);
+
+        correlationIdList.add(userDTO.getUuid());
     }
 
-    private void checkAndOpenDepositForUserByUuid(UUID uuid){
+    private void checkAndOpenDepositForUserByUuid(UUID uuid) {
         final PersonalData personalData =
                 personalDataService.getPersonalByForeignKey(userService.
                         getUserByLogin(LoginInfoService.data.get(uuid)).getId());
